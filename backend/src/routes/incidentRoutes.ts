d0b128aa8f30326ebe8c7792d90966909909a1d1
SaiAmirthesh <saiamirthesh8419@gmail.com>
2026-09-02 04:11:06 +0530

    feat: incident Routes changes

--- backend/src/routes/incidentRoutes.ts
+++ backend/src/routes/incidentRoutes.ts
@@ -4,8 +4,7 @@ import { requireSupabaseAuth } from '../middleware/authMiddleware.js';
 
 export const incidentRouter = Router();
 
-incidentRouter.post('/media', createFromMediaIncident); // Must be before /:id param routes
+incidentRouter.post('/media', requireSupabaseAuth, createFromMediaIncident);
 incidentRouter.get('/', listIncidents);
 incidentRouter.get('/:id', getIncidentById);
 incidentRouter.post('/', requireSupabaseAuth, createAndDispatchIncident);
-incidentRouter.post('/media', requireSupabaseAuth, createFromMediaIncident);
